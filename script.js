6d0ab8f65b6fd133ec89405519c61754df8cbe51
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-04-01 02:07:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+var fontsize = document.getElementById("fontsize");
+var fontcolor = document.getElementById("fontcolor");
+
+var mycookies = document.cookie.split("; ")
+mycookies.map((item)=>{
+	var cookieList = item.split("=");
+	
+	cookieList.map((para)=>{
+	fontsize.value = para[1];
+	fontcolor.value = para[3];
+		
+	})
+	
+})
+
+function onSubmit(event){
+	document.cookie = `fontsize=${fontsize.value}`;
+	document.cookie = `fontcolor=${fontcolor.value}`;
+	event.preventDefault();
+}
